b0eed10df4dcbf7f843fdea609e96d866ef39849
Pariterre <pariterre@hotmail.com>
2023-08-16 17:38:05 -0400

    Added nshooting

--- bioptim_gui/core/bioptim_gui.py
+++ bioptim_gui/core/bioptim_gui.py
@@ -1,13 +1,15 @@
 import sys
 
 # TODO port to PyQt6
+from PyQt5.QtCore import QLocale
+from PyQt5.QtGui import QDoubleValidator, QIntValidator
 from PyQt5.QtWidgets import (
     QApplication,
     QMainWindow,
     QWidget,
     QGridLayout,
     QHBoxLayout,
-    QVBoxLayout,
+    QLineEdit,
     QComboBox,
     QPushButton,
     QFileDialog,
@@ -17,6 +19,9 @@ from PyQt5.QtWidgets import (
 from .ocp_exporter import OcpExporter
 from ..bioptim_interface.bio_model import BioModels
 from ..bioptim_interface.optimal_control_type import OptimalControlType
+
+
+QLocale.setDefault(QLocale(QLocale.English))
 
 
 class BioptimGui(QMainWindow):
@@ -39,6 +44,18 @@ class BioptimGui(QMainWindow):
 
     _bio_model_path_qlabel: QLabel
 
+    _phase_time: float
+
+    @property
+    def phase_time(self):
+        return self._phase_time
+
+    _n_shooting: int
+
+    @property
+    def n_shooting(self):
+        return self._n_shooting
+
     @property
     def bio_model_path(self):
         return self._bio_model_path_qlabel.text()
@@ -50,8 +67,10 @@ class BioptimGui(QMainWindow):
 
         self._build_select_ocp_type_combo_box(0, 0)
         self._build_select_bio_model_protocol_combo_box(1, 0)
-        self._build_bio_model_path_text_line(2, 0)
-        self._build_generate_ocp_button(3, 0)
+        self._build_bio_model_path_selection(2, 0)
+        self._build_phase_time_declaration(3, 0)
+        self._build_n_shooting_declaration(4, 0)
+        self._build_generate_ocp_button(5, 0)
 
     def exec(self):
         self.show()
@@ -85,7 +104,7 @@ class BioptimGui(QMainWindow):
         combo_box.currentIndexChanged.connect(on_protocol_selected)
         self._central_grid_layout.addWidget(combo_box, row, col)
 
-    def _build_bio_model_path_text_line(self, row: int, col: int):
+    def _build_bio_model_path_selection(self, row: int, col: int):
         def select_file():
             selection = QFileDialog.getOpenFileName(
                 filter=f"{self.bio_model_protocol} (*.{self._bio_models_extensions[self._bio_models_current_index]})"
@@ -104,6 +123,42 @@ class BioptimGui(QMainWindow):
 
         self._central_grid_layout.addLayout(select_file_layout, row, col)
 
+    def _build_phase_time_declaration(self, row: int, col: int):
+        def on_text_changed(text):
+            if text == "":
+                self._phase_time = 0
+            else:
+                self._phase_time = float(text)
+
+        layout = QHBoxLayout()
+        phase_time_label = QLabel("Phase time")
+        text_edit = QLineEdit()
+        text_edit.setValidator(QDoubleValidator())
+        text_edit.textChanged.connect(on_text_changed)
+
+        layout.addWidget(phase_time_label)
+        layout.addWidget(text_edit)
+
+        self._central_grid_layout.addLayout(layout, row, col)
+
+    def _build_n_shooting_declaration(self, row: int, col: int):
+        def on_text_changed(text):
+            if text == "":
+                self._n_shooting = 0
+            else:
+                self._n_shooting = int(text)
+
+        layout = QHBoxLayout()
+        shooting_point_label = QLabel("Number of shooting points")
+        text_edit = QLineEdit()
+        text_edit.setValidator(QIntValidator())
+        text_edit.textChanged.connect(on_text_changed)
+
+        layout.addWidget(shooting_point_label)
+        layout.addWidget(text_edit)
+
+        self._central_grid_layout.addLayout(layout, row, col)
+
     def _build_generate_ocp_button(self, row: int, col: int):
         def on_click():
             self._generate_ocp_file()
@@ -115,10 +170,14 @@ class BioptimGui(QMainWindow):
     def _generate_ocp_file(self):
         selection = QFileDialog.getSaveFileName(filter="Python Files (*.py)")
         filename = selection[0]
+        if filename is None or filename == "":
+            return
 
         exporter = OcpExporter(
             optimal_control_type=self.optimal_control_type,
             bio_model_protocol=self.bio_model_protocol,
             bio_model_path=self.bio_model_path,
+            phase_time=self.phase_time,
+            n_shooting=self.n_shooting,
         )
         exporter.export(filename)
